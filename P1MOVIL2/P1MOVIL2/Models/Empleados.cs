8cc83173ee69556063550eae2745b00cd8b9dbe9
danie <danie@DANIEL-GB>
2022-02-11 02:36:29 -0600

    Add project files.

---
+++ P1MOVIL2/P1MOVIL2/Models/Empleados.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+namespace P1MOVIL2.Models
+{
+    public class Empleados
+    {
+        [PrimaryKey, AutoIncrement]
+        public int codigo { get; set; }
+        [MaxLength(100)]
+        public String nombre { get; set; }
+        [MaxLength(100)]
+        public String apellidos { get; set; }
+        public Int32 edad { get; set; }
+        public DateTime fecha_nac { get; set; }
+        [MaxLength(100)]
+        public String ocupacion { get; set; }
+
+        public String correo { get; set; }
+
+        public String direccion { get; set; }
+
+
+    }
+}
